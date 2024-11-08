218505374a2f1e7722bb8250954c538b31a2528f
Rahul Soni <rahulsoni.myventures@gmail.com>
2024-11-09 00:19:44 +0530

    Logs added

--- src/main/java/com/systemdesign/tutorials/controllers/TestController.java
+++ src/main/java/com/systemdesign/tutorials/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.systemdesign.tutorials.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api1")
 public class TestController {
 
+    private static final Logger logger = LogManager.getLogger(TestController.class);
+
     // A simple GET request
     @GetMapping("/hello")
     public String sayHello() {
+        logger.info("Api /hello is invoked to say - Hello, World!.");
         return "Service 01 says - Hello, World!";
     }
 
